38163c7031cf113c5e7a3adb9ef76a274735a8fe
Antônio Augusto Boaventura Mariano <antonioboaventura057@gmail.com>
2026-04-08 22:19:52 -0300

    Create redirecionamento.sh

---
+++ Semana 08/redirecionamento.sh
@@ -0,0 +1,12 @@
+  GNU nano 6.2                                                     redirecionamento.sh                                                               
+#!/bin/bash
+clear
+# Listagem das interfaces de redes e endereços de rede e endereços IP
+# gravadas em um arquivo chamado net_interfaces
+
+ip address > net_interfaces
+
+
+
+
+
